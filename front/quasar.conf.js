33a8a553aa1d40a71ba66ce3cdac6d268b31fb2c
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-09-14 17:58:51 -0500

    Router config

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -44,7 +44,7 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
 
@@ -77,7 +77,7 @@ module.exports = function (/* ctx */) {
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
